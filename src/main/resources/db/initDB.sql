b7c799b1aa01d2ce101fdb76c032442d9dceb4dc
Walliword <wallianw@gmail.com>
2018-10-24 11:56:40 +0300

    HW3 2 meals DB created and filled

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -1,8 +1,10 @@
+DROP TABLE IF EXISTS meals;
 DROP TABLE IF EXISTS user_roles;
 DROP TABLE IF EXISTS users;
 DROP SEQUENCE IF EXISTS global_seq;
 
-CREATE SEQUENCE global_seq START 100000;
+CREATE SEQUENCE global_seq
+  START 100000;
 
 CREATE TABLE users
 (
@@ -14,7 +16,8 @@ CREATE TABLE users
   enabled          BOOL DEFAULT TRUE       NOT NULL,
   calories_per_day INTEGER DEFAULT 2000    NOT NULL
 );
-CREATE UNIQUE INDEX users_unique_email_idx ON users (email);
+CREATE UNIQUE INDEX users_unique_email_idx
+  ON users (email);
 
 CREATE TABLE user_roles
 (
@@ -23,3 +26,14 @@ CREATE TABLE user_roles
   CONSTRAINT user_roles_idx UNIQUE (user_id, role),
   FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
+
+CREATE TABLE meals
+(
+  id          INTEGER PRIMARY KEY default nextval('global_seq'),
+  user_id     INTEGER                 NOT NULL,
+  date_time   TIMESTAMP DEFAULT now() NOT NULL,
+  description VARCHAR                 NOT NULL,
+  calories    INTEGER                 NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+);
+CREATE UNIQUE INDEX meals_user_date ON meals(user_id, id, date_time);
--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,3 +1,4 @@
+DELETE FROM meals;
 DELETE FROM user_roles;
 DELETE FROM users;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
@@ -9,3 +10,13 @@ INSERT INTO users (name, email, password) VALUES
 INSERT INTO user_roles (role, user_id) VALUES
   ('ROLE_USER', 100000),
   ('ROLE_ADMIN', 100001);
+
+INSERT INTO meals (user_id, date_time, description, calories) VALUES
+                                                                     (100000, '2018-10-23 09:00:00', 'Завтрак', 500),
+                                                                     (100000, '2018-10-23 14:00:00', 'Обед', 500),
+                                                                     (100000, '2018-10-23 20:00:00', 'Ужин', 1000),
+                                                                     (100000, '2018-10-24 09:00:00', 'Завтрак', 500),
+                                                                     (100000, '2018-10-24 14:00:00', 'Обед', 501),
+                                                                     (100000, '2018-10-24 20:00:00', 'Ужин', 1000),
+                                                                     (100001, '2018-10-23 12:00:00', 'еда админская', 1500),
+                                                                     (100001, '2018-10-24 12:00:00', 'еда админская', 1500)
